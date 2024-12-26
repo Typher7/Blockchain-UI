c25ede64c09231f0c74930e09eb767b966983623
Typher7 <111155443+Typher7@users.noreply.github.com>
2024-12-26 01:09:45 -0600

    name fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import BlockchainViewer from './components/BlockchainViewer';
 import Navbar from './components/NavBar';
 import { Footer } from "./components/Footer";
 import Step from './components/Steps';
-import Test from './components/Blocks';
+import Blocks from './components/Blocks';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Test />
+      <Blocks />
       <Footer />
     </div>
   );
