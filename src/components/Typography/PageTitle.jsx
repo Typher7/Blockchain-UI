472d3e12e5d2662307442a035da80337b9325882
Typher7 <111155443+Typher7@users.noreply.github.com>
2024-12-26 01:04:16 -0600

    blocks table components

---
+++ src/components/Typography/PageTitle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function PageTitle({ children }) {
+  return (
+    <h1 className="my-6 text-2xl font-bold text-gray-700">{children}</h1>
+  )
+}
+
+
+export default PageTitle
