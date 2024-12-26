f32a4fb3b7e4ead4426bd058693d89737580f30c
Typher7 <111155443+Typher7@users.noreply.github.com>
2024-12-26 01:05:06 -0600

    config files update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Windmill } from '@windmill/react-ui';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Windmill>
+      <App />
+    </Windmill>
   </React.StrictMode>
 );
