0f6f56a84a69b9f162138baacc525c1cf9fd9a23
Typher7 <111155443+Typher7@users.noreply.github.com>
2024-12-28 00:23:12 -0600

    hero and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/NavBar';
 import { Footer } from "./components/Footer";
 import Blocks from './components/Blocks';
+import Hero  from './components/Hero';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
       <Blocks />
       <Footer />
     </div>
